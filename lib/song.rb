e665e845921b930f64bc528fc7c25367f4c52e9f
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:22:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ attr_accessor :name, :artist
   	  def self.new_by_filename(filename)
       song = self.new
     song_name = filename.split(" - ")[1]
-    song.artist_name = filename.split(" - ")[0]
+    song.artist = filename.split(" - ")[0]
     song.artist = Artist.find_or_create_by_name(name)
      song
   end
