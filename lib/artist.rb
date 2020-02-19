3b6747d6ba6fd9960e95f533561e50d88932de5d
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:27:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,10 +12,10 @@ class Artist
                           #stored in @songs
   end
 
-  #def save
-   # @@all << self     #saves all created instances of Artist class in 
+  def save
+    @@all << self     #saves all created instances of Artist class in 
                       #@@all class variable
-  #end
+  end
 
   def self.all      #class method all returns all instances of Artist class 
     @@all
