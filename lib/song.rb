c90e2385a506ebdd3f252c5588139c5f176b9a41
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:44:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,40 @@
 class Song
 
-  attr_accessor :name, :artist
+    @@all = []
 
-  def initialize(name)
-    @name = name
-  end
+    attr_accessor :artist, :name
 
-  def self.new_by_filename(file)
-    song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    song = self.new(song_name)
-    #this is calling the #artist_name=
-    #it is a writer method that assigns that variable to artist
-    #you are not just assigning the string to artist, because you want the artist attribute to be an artist instance instead
-    song.artist_name = artist
-    song
-  end
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
-  #this is a setter method, a writer,
-  #it sets the artist attribute of the song instance equal to an artist instance (rather than just a string)
-  #it calls a Class Method in Artist, that returns an artist object
-  #the find_or_create_by_name either finds an existing artist and returns it, or creates a new object and returns it
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    #after the artist instance is returned
-    #this method calls the artist instance method #add_song
-    #add_song adds this current song instance to the artist's song list
-    self.artist.add_song(self)
-  end
+    def self.all
+        @@all
+    end
+
+    def artist_name=(name)
+        #Song.all.detect {|artist| artist.name == name} || new_artist = Artist.new(name)
+        #self.artist = new_artist
+        #OR, though not sure if ^ passes
+        # new_artist = Artist.find_or_create_by_name(name)
+        # new_artist.add_song(self)
+        # self.artist=(new_artist)
+        #OR
+        # self.artist=(Artist.find_or_create_by_name(name))
+        # artist.add_song(self)
+        #OR
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
+
+    end
+
+    def self.new_by_filename(filename)
+        artist = filename.split(" - ")[0]
+        song = filename.split(" - ")[1]
+        new_song = Song.new(song)
+        new_song.artist=(Artist.new(artist))
+        new_song
+    end
 
 end
