3be7a8944c956711ee2e8f0e5e05e23a217b6261
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,40 +1,21 @@
 class Song
+  attr_accessor :name, :artist
 
-    @@all = []
+  def initialize(name)
+    @name = name
+  end
 
-    attr_accessor :artist, :name
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
+  end
 
-    def initialize(name)
-        @name = name
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-
-    def artist_name=(name)
-        #Song.all.detect {|artist| artist.name == name} || new_artist = Artist.new(name)
-        #self.artist = new_artist
-        #OR, though not sure if ^ passes
-        # new_artist = Artist.find_or_create_by_name(name)
-        # new_artist.add_song(self)
-        # self.artist=(new_artist)
-        #OR
-        # self.artist=(Artist.find_or_create_by_name(name))
-        # artist.add_song(self)
-        #OR
-        self.artist = Artist.find_or_create_by_name(name)
-        artist.add_song(self)
-
-    end
-
-    def self.new_by_filename(filename)
-        artist = filename.split(" - ")[0]
-        song = filename.split(" - ")[1]
-        new_song = Song.new(song)
-        new_song.artist=(Artist.new(artist))
-        new_song
-    end
-
-end
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+end 
