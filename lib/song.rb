c655faa0519a7493204f81b544b3eb2ece9d9be9
sasha walker <sashawalker31@gmail.com>
2020-02-19 23:23:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,19 @@
-class Song 
-attr_accessor :name, :artist
+class Song
+  attr_accessor :artist, :name
 
-
-	  def initialize(name)
-    	    @name = name
-    end
-
-
-  	  def self.new_by_filename(filename)
-      song = self.new
-    song_name = filename.split(" - ")[1]
-    song.artist = filename.split(" - ")[0]
-    song.artist = Artist.find_or_create_by_name(name)
-     song
+  def initialize(name)
+    @name = name
   end
-end 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+end
